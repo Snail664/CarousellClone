50a0f01d93be1dee1e8ec0ec16cf7b6039b34e85
root <root@DESKTOP-E4RRN9V.localdomain>
2020-10-01 13:34:42 +0800

    Changes to fresh finds

--- carousell-clone/src/Listing.js
+++ carousell-clone/src/Listing.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import './Listing.css'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Listing(props) {
-    return <div className='listing'>
-        <div className='listing__header'>
-            <img className='header__left' src={process.env.PUBLIC_URL + '/images/' + props.profilePic} />
-            <div className='header__right'>
-                <p className='listing__username'>{ props.username }</p>
-                <p className='listing__time'>{ props.time }</p>
+    return (
+         <div className='listing'>
+            <div className='listing__header'>
+                <img className='header__left' src={process.env.PUBLIC_URL + '/images/' + props.profilePic} />
+                <div className='header__right'>
+                    <p className='listing__username'>{ props.username }</p>
+                    <p className='listing__time'>{ props.time }</p>
+                </div>
             </div>
-        </div>
-        <img className='listing__thumbnail' src={props.thumbnail} />
-        <p className='listing__title'>{ props.title }</p>
-        <p className='listing__description'>{ props.description }</p>
+            <img className='listing__thumbnail' src={props.thumbnail} />
+            <p className='listing__title'>{ props.title }</p>
+            <p className='listing__description'>{ props.description }</p>
+            <div className='listing__footer'>
+                <FavoriteBorderIcon className='listing__like' />
+                <MoreVertIcon className='listing__more' />
+            </div>
     </div>
-}
+    )}
 
 export default Listing
