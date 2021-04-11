f7419fbc2d24515561e5d067feca09578789c06d
Chiraag Anand <67359421+Snail664@users.noreply.github.com>
2021-04-11 11:51:58 +0800

    styling updates

--- carousell-clone/src/FreshFinds.js
+++ carousell-clone/src/FreshFinds.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import './FreshFinds.css'
-import { listings } from './listingsDummyData'
-import Listing from './Listing'
+import React from "react";
+import "./FreshFinds.css";
+import { listings } from "./listingsDummyData";
+import Listing from "./Listing";
 
 function FreshFinds() {
-    return (
-        <div className='freshfinds'>
-            <p className='freshfinds__title'>Fresh Finds</p>
-            <div id='listings' className='listings'>
-            <Listing profilePic='defaultProfilePic.png' username={listings[2].username} thumbnail={listings[2].thumbnail} title={listings[2].title} description={listings[2].description} time='1000' />
-            </div>
-        </div>
-        
-    )
-};
+  return (
+    <div className="freshfinds">
+      <p className="freshfinds__title">Fresh Finds</p>
+      <div id="listings" className="listings">
+        {listings.map((listing) => {
+          return (
+            <Listing
+              profilePic="defaultProfilePic.png"
+              username={listing.username}
+              thumbnail={listing.thumbnail}
+              title={listing.title}
+              description={listing.description}
+              time="1000"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default FreshFinds;
--- carousell-clone/src/Header.js
+++ carousell-clone/src/Header.js
@@ -1,74 +1,79 @@
-import React from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
-import Searchbar from './Searchbar'
-import { categories } from './categoriesObj'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import Searchbar from "./Searchbar";
+import { categories } from "./categoriesObj";
 
 function Header() {
-    var selectedId
-    var category = categories[selectedId]
-    var i = 0;
-    function overDropdown(e) {
-        selectedId = e.target.id
-        category = categories[selectedId]
-        while (i < category.length) {
-            document.getElementById('dropdown').style.visibility = 'visible'
-            document.getElementById('dropdown').innerHTML += `<div id='`+category[i].title+`'class='dropdown__container'>
-                <img class='dropdown__icon' src='`+process.env.PUBLIC_URL + category[i].icon+`' />
-                <p class='dropdown__title'>
-                    `+category[i].title+`
-                </p>
-            </div>`
-            for (let k = 0; k < category[i].options.length; k++) {
-                if (category[i]) {
-                    var option = document.createElement('p')
-                    option.setAttribute('class', 'option')
-                    var optionContent = document.createTextNode(category[i].options[k])
-                    option.appendChild(optionContent)
-                    document.getElementById(category[i].title).appendChild(option)
-                }
-            }
-            i++
-        }
-        }
-    function exitDropDown(e) {
-            document.getElementById('dropdown').innerHTML = ''
-            document.getElementById('dropdown').style.visibility = 'hidden'
-            i = 0
+  return (
+    <nav className="header">
+      <div className="header__topNavContainer">
+        <div className="header__topNav">
+          <div className="logo__container">
+            <img
+              className="logoWhite"
+              src={process.env.PUBLIC_URL + "/images/carousell-logo-white.PNG"}
+            />
+            <p className="line">|</p>
+          </div>
+          <div className="categories">
+            <Link className="link" to="/">
+              <div id="Cars & Property" className="category__item">
+                Cars & Property
+              </div>
+            </Link>
+            <Link className="link" to="/">
+              <div id="Fashion" className="category__item">
+                Fashion
+              </div>
+            </Link>
+            <Link className="link" to="/">
+              <div id="Home & Living" className="category__item">
+                Home & Living
+              </div>
+            </Link>
+            <Link className="link" to="/">
+              <div id="Mobiles & Electronics" className="category__item">
+                Mobiles & Electronics
+              </div>
+            </Link>
+            <Link className="link" to="/">
+              <div id="Hobbies & Games" className="category__item">
+                Hobbies & Games
+              </div>
+            </Link>
+            <Link className="link" to="/">
+              <div id="Jobs & Services" className="category__item">
+                Jobs & Services
+              </div>
+            </Link>
+            <Link className="link" to="/">
+              <div id="Others" className="category__item">
+                Others
+              </div>
+            </Link>
+          </div>
+          <div className="topNav__right">
+            <p className="right__item">Register</p>
+            <p className="right__item">Login</p>
+          </div>
+        </div>
+      </div>
+      <div id="dropdown" className="dropdown"></div>
+      <div className="header__bottomNavContainer">
+        <div className="header__bottomNav">
+          <img
+            className="logoRed"
+            src={process.env.PUBLIC_URL + "/images/carousell-logo-red.PNG"}
+          />
+          <Searchbar />
+          <button type="submit" className="sellButton">
+            Sell
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
-    return (
-        <nav className='header'>
-            <div className='header__topNavContainer'>
-                <div className='header__topNav'>
-                    <div className='logo__container'>
-                        <img className='logoWhite' src={process.env.PUBLIC_URL + '/images/carousell-logo-white.PNG'} />
-                        <p className='line'>|</p>
-                    </div>
-                    <div className='categories'>
-                        <Link className='link' to='/'><div id='Cars & Property' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Cars & Property</div></Link>
-                        <Link className='link' to='/'><div id='Fashion' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Fashion</div></Link>
-                        <Link className='link' to='/'><div id='Home & Living' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Home & Living</div></Link>
-                        <Link className='link' to='/'><div id='Mobiles & Electronics' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Mobiles & Electronics</div></Link>
-                        <Link className='link' to='/'><div id='Hobbies & Games' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Hobbies & Games</div></Link>
-                        <Link className='link' to='/'><div id='Jobs & Services' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Jobs & Services</div></Link>
-                        <Link className='link' to='/'><div id='Others' onMouseOver={overDropdown} onMouseOut={exitDropDown} className='category__item'>Others</div></Link>
-                    </div>
-                    <div className='topNav__right'>
-                        <p className='right__item'>Register</p>
-                        <p className='right__item'>Login</p>
-                    </div>
-                </div>
-            </div>
-            <div id='dropdown' className='dropdown'></div>
-            <div className='header__bottomNavContainer'>
-                <div className='header__bottomNav'>
-                    <img className='logoRed' src={process.env.PUBLIC_URL + '/images/carousell-logo-red.PNG'} />
-                    <Searchbar />
-                    <button type='submit' className='sellButton'>Sell</button>
-                </div>
-            </div>
-        </nav>
-    )
-};
 
 export default Header;
