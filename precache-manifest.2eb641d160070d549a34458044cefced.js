dd25cd59f1a6084f5a95053ddb8d3627362ca59c
Chiraag Anand <67359421+Snail664@users.noreply.github.com>
2021-04-11 12:04:21 +0800

    Updates

---
+++ precache-manifest.2eb641d160070d549a34458044cefced.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64f1396ca8da5b990ccb10539171179",
+    "url": "/CarousellClone/index.html"
+  },
+  {
+    "revision": "8b80d46911110c14524b",
+    "url": "/CarousellClone/static/css/2.3ccac6ac.chunk.css"
+  },
+  {
+    "revision": "89e381d50ce678b6d302",
+    "url": "/CarousellClone/static/css/main.e5654399.chunk.css"
+  },
+  {
+    "revision": "8b80d46911110c14524b",
+    "url": "/CarousellClone/static/js/2.31a8bc6b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CarousellClone/static/js/2.31a8bc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e381d50ce678b6d302",
+    "url": "/CarousellClone/static/js/main.47f58d6d.chunk.js"
+  },
+  {
+    "revision": "e51fba15cb51df75eed2",
+    "url": "/CarousellClone/static/js/runtime-main.79f15dca.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/CarousellClone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/CarousellClone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/CarousellClone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/CarousellClone/static/media/slick.f97e3bbf.svg"
+  }
+]);
